e4b05bade91672ccc9db498b608c633feb894655
antho6229 <antho6229@7KSM6Y1.Student.UGDSB.ED>
2018-10-03 10:26:06 -0400

    Stuff

--- src/u1_a2_p3_incometax/U1_A2_P3_IncomeTax.java
+++ src/u1_a2_p3_incometax/U1_A2_P3_IncomeTax.java
@@ -28,6 +28,11 @@ public class U1_A2_P3_IncomeTax {
         final double FOURTH_TAX_RATE = 0.29;
         final double FIFTH_TAX_RATE = 0.33;
         
+        final double FIRST_BRACKET = 46605;
+        final double SECOND_BRACKET = 93208;
+        final double THIRD_BRACKET = 144489;
+        final double FOURTH_BRACKT = 205842;
+        
         //create Scanner and DecimalFormat
         Scanner keyedInput = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("$###.##");
@@ -35,8 +40,28 @@ public class U1_A2_P3_IncomeTax {
         //declare variables
         double salary, incomeTax;
         
+        incomeTax = 0;
+        
         //get salary
         System.out.println("Enter your salary:");
+        salary = keyedInput.nextInt();
+        
+        if(salary > 46605){
+            incomeTax += 46605 * FIRST_TAX_RATE;
+            if(salary > 93208){
+                incomeTax += 93208 * SECOND_TAX_RATE;
+                if(salary > 144489){
+                    incomeTax += 144489 * THIRD_TAX_RATE;
+                    if(salary > 205842){
+                        
+                    }
+                }
+            }else{
+                incomeTax += (salary - 93208) * SECOND_TAX_RATE;
+            }
+        }else{
+            incomeTax = salary * FIRST_TAX_RATE;
+        }
     }
     
 }
